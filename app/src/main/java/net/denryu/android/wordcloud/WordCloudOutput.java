2877ac74e805d67e6f424c8a17d1a040cc54e3ee
JosephWanderer <jwande02@seattlecentral.edu>
2017-08-07 01:13:24 -0700

    Add logs, try setScale

--- app/src/main/java/net/denryu/android/wordcloud/WordCloudOutput.java
+++ app/src/main/java/net/denryu/android/wordcloud/WordCloudOutput.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
 
 //import net.alhazmy13.example.R;
 import net.alhazmy13.wordcloud.ColorTemplate;
@@ -17,18 +19,26 @@ import java.util.Random;
 public class WordCloudOutput extends AppCompatActivity {
     private static final String TAG = "WordCloudActivity";
     List<WordCloud> list ;
-    String text = "word word word word word word word word word word word word word word word word word word different";
+    String text = "0 1 2 3 4 5 6 7 8 9 a b c d e f";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wordcloud_output);
+        Log.d("WordCloudOutput", "OnCreate");
         generateRandomText();
+
         WordCloudView wordCloud = (WordCloudView) findViewById(R.id.wordCloud);
+        wordCloud.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
         wordCloud.setDataSet(list);
-        wordCloud.setSize(300,300);
+        //wordCloud.setSize(wordCloud.getMeasuredWidth(),wordCloud.getMeasuredHeight());
+        wordCloud.setSize(500,500);
         wordCloud.setColors(ColorTemplate.MATERIAL_COLORS);
+        wordCloud.setScale(20,10);
         wordCloud.notifyDataSetChanged();
+        Log.d("WordCloudOutput", "wordCloud: " + wordCloud.toString());
 
     }
 
@@ -37,7 +47,9 @@ public class WordCloudOutput extends AppCompatActivity {
         list = new ArrayList<>();
         Random random = new Random();
         for (String s : data) {
-            list.add(new WordCloud(s,random.nextInt(50)+10));
+            WordCloud w = new WordCloud(s,random.nextInt(2) + 10001);
+            Log.d("WordList", "text: " + w.getText() + " Weight: " + w.getWeight());
+            list.add(w);
         }
     }
 }
